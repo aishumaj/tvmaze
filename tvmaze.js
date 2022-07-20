0f77dc4abbbe4d75981e100b2ae7670b3b1cfd19
Emily Chen <sticky_sugar@hotmail.com>
2022-07-20 12:36:34 -0700

    complete getShowsByTerm

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,8 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const API_URL = "http://api.tvmaze.com/search/shows";
+const EPISODE_SEARCH = "http://api.tvmaze.com/shows";
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,28 +14,48 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const showSearch = await axios.get(API_URL, { params: { q: term } });
+  let listOfShows = [];
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  for (let i = 0; i < showSearch.data.length; i++) {
+    let image = "https://friconix.com/png/fi-cnsuxx-question-mark.png";
+    if (showSearch.data[i].show.image.original !== null) {
+      image = image = showSearch.data[i].show.image.original;
     }
-  ]
-}
+
+    listOfShows.push({
+      id: showSearch.data[i].show.id,
+      name: showSearch.data[i].show.name,
+      summary: showSearch.data[i].show.summary,
+      image
+    });
+
+    return listOfShows;
+  }
+};
+
+
+//   return [
+//     {
+//       id: 1767,
+//       name: "The Bletchley Circle",
+//       summary:
+//         `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+//            women with extraordinary skills that helped to end World War II.</p>
+//          <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+//            normal lives, modestly setting aside the part they played in
+//            producing crucial intelligence, which helped the Allies to victory
+//            and shortened the war. When Susan discovers a hidden code behind an
+//            unsolved murder she is met by skepticism from the police. She
+//            quickly realises she can only begin to crack the murders and bring
+//            the culprit to justice with her former friends.</p>`,
+//       image:
+//           "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+//     }
+//   ]
+// }
 
 
 /** Given list of shows, create markup for each and to DOM */
@@ -43,11 +65,11 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -56,11 +78,12 @@ function populateShows(shows) {
                Episodes
              </button>
            </div>
-         </div>  
+         </div>
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
